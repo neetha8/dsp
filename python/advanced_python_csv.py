4f0bb461b8b87ecf1a61ef0d451fed7bdb806887
neetha8 <Neetha.chilukuri@gmail.com>
2017-03-18 22:28:57 -0400

    Update advanced_python_csv.py
    code to write emails to the csv file

--- python/advanced_python_csv.py
+++ python/advanced_python_csv.py
@@ -1,1 +1,16 @@
-PLACE YOUR CODE HERE
+import csv
+
+lst_emails = []
+f = open('faculty.csv')
+csv_file = csv.reader(f)
+
+# skip the header row
+next(csv_file)
+# add emails to the list 
+for row in csv_file:
+    lst_emails.append(row[3])  
+# write emails list to emails.csv
+with open("emails.csv", "w") as f:
+    writer = csv.writer(f)
+    for email in lst_emails:
+        writer.writerow([email]) 
