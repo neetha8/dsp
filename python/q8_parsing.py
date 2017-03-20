2d19bce533174b2ca612fac0bbbd64c5d2f56fdc
neetha8 <Neetha.chilukuri@gmail.com>
2017-03-19 22:19:42 -0400

    Update q8_parsing.py
    Function to parse the csv file. 
    Team with least difference in goals is Leicester.

--- python/q8_parsing.py
+++ python/q8_parsing.py
@@ -4,3 +4,17 @@
 # against opponents, and had 36 goals scored against them). Write a program to read the file, 
 # then print the name of the team with the smallest difference in ‘for’ and ‘against’ goals.
 
+import csv
+import re
+
+input_file = csv.DictReader(open('football.csv'))
+min_diff,team_name  = None, None
+
+for row in input_file:
+    diff = int(row['Goals']) -  int(row['Goals Allowed'])
+    if min_diff == None or min_diff > diff:
+        min_diff = diff
+        team_name = row['Team']
+
+print ("Team with least difference in goals is %s." %team_name)
+
